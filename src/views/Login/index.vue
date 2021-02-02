d2a5acbcf35425386f860e5374942e451c32fb10
sangyan <sangyan@ysstech.com>
2021-02-02 18:33:57 +0800

    新增:增加login页面主页

---
+++ src/views/Login/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class='Login'>
+        用户名<el-input v-model="input" placeholder="请输入内容"></el-input>
+        <el-input v-model="input" placeholder="请输入内容"></el-input>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+
+            }
+        },
+        mounted(){
+        },
+        computed:{
+        },
+        methods:{
+        }
+    }
+</script>
+
+<style>
+</style>
