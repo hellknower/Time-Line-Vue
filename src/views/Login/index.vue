30ad255adc6f395e37667de9ee05e0e4d1908514
sangyan <sangyan@ysstech.com>
2021-02-04 10:23:14 +0800

    新增:添加登录界面的简单样式

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,20 +1,22 @@
 <template>
     <div id="Login">
         <div class="login-input-position">
-            <div class="login-input login-userName">
-                <span>用户名:</span>
-                <el-input v-model="userName" placeholder="请输入内容"/>
-            </div>
-            <div class="login-input login-passWord">
-                <span>密码:</span>
-                <el-input v-model="passWord" placeholder="请输入内容"/>
-            </div>
-            <div class="create-or-forget-buttonArray">
-                <el-button type="text" @click="forget">忘记密码</el-button>
-                <el-button type="text">注册账号</el-button>
-            </div>
-            <div class="login-button">
-                <el-button type="primary" @click="login">登陆</el-button>
+            <div class="login-input--center-position">
+                <div class="login-input login-userName">
+                    <span>用户名:</span>
+                    <el-input v-model="userName" clearable placeholder="请输入内容"/>
+                </div>
+                <div class="login-input login-password">
+                    <span>密码:</span>
+                    <el-input v-model="password" clearable placeholder="请输入内容"/>
+                </div>
+                <div class="create-or-forget-buttonArray">
+                    <el-button type="text" @click="forget">忘记密码</el-button>
+                    <el-button type="text">注册账号</el-button>
+                </div>
+                <div class="login-button">
+                    <el-button type="primary" @click="login">登陆</el-button>
+                </div>
             </div>
         </div>
     </div>
@@ -25,7 +27,7 @@
         data(){
             return {
                 userName:'',
-                passWord:''
+                password:''
             }
         },
         mounted(){
@@ -53,12 +55,34 @@
 
 <style lang="stylus" rel="stylesheet/stylus">
 #Login
+    height:100%
     .login-input-position
-        margin:0 auto
         width:500px
-    .login-input
+        height:350px
+        margin:0 auto
+        box-shadow:5px 5px 5px 5px gray
+        text-align:center
+        position:relative
+        top:50%
+        transform:translateY(-50%)
         display:flex
-        span
-            width:200px
-            text-align:right
+        flex-direction:column   
+        justify-content:center
+        .login-input
+            display:flex
+            justify-content:center
+            span
+                width:80px
+                margin-right:8px
+                line-height:40px
+                text-align:right
+            .el-input
+                width:240px
+        .login-userName
+            margin-bottom:32px
+        .login-password
+            margin-bottom:16px
+        .create-or-forget-buttonArray
+            margin-bottom:16px
+
 </style>
