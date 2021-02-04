442e7add41f7fea3bb190adbaaa65f2b08ba35d0
sangyan <sangyan@ysstech.com>
2021-02-04 14:15:04 +0800

    修改:修改输入框提示信息为框下文字显示 新增:增加点击登录按钮对输入框的校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -10,8 +10,11 @@
                         clearable 
                         placeholder="请输入用户名"
                         @blur="checkUsername"
-                        @keydown.enter.native="pressEnter"/>
+                        @keydown.enter.native="login"/>
                 </div>
+                <div class="error-message">
+                    <span v-show="!isUsernameRight">{{usernameErrorMsg}}</span>
+                </div>  
                 <div class="login-input login-password">
                     <span>密码:</span>
                     <el-input 
@@ -21,8 +24,12 @@
                         placeholder="请输入密码" 
                         show-password
                         @blur="checkPassword"
-                        @keydown.enter.native="pressEnter"/>
+                        @keydown.enter.native="login"/>
                 </div>
+                <div class="error-message">
+                    <span v-show="!isPasswordRight">{{passwordErrorMsg}}</span>
+                    
+                </div>                
                 <div class="create-or-forget-buttonArray">
                     <el-button type="text" @click="forget" class="forget-password">忘记密码</el-button>
                     <el-button type="text">注册账号</el-button>
@@ -43,6 +50,8 @@
                 password:'',
                 isUsernameRight:false,
                 isPasswordRight:false,
+                usernameErrorMsg:'',
+                passwordErrorMsg:'',
             }
         },
         mounted(){
@@ -51,65 +60,41 @@
         },
         methods:{
             checkUsername(event){
-                const username = event.target.value;
+                const username = typeof event === 'object'?event.target.value:event;
                 if(username.length < 1){
-                    this.$message({
-                        type:'error',
-                        message:'用户名不能为空'
-                    })
+                    this.isUsernameRight = false;
+                    this.usernameErrorMsg = '用户名不能为空';
                     return;
                 }else if(username.length > 18){
-                    this.$message({
-                        type:'error',
-                        message:'用户名为数不能大于18位'
-                    })
+                    this.isUsernameRight = false;
+                    this.usernameErrorMsg = '用户名为数不能大于18位';
                     return;
                 }
                 const rex = new RegExp(/^[a-z,A-Z,0-9,_,\u4e00-\u9fa5]{1,18}$/);
                 this.isUsernameRight = rex.test(username);
                 if(!this.isUsernameRight){
-                    this.$message({
-                        type:'error',
-                        message:'账号格式不正确,请输入正确的账号',
-                    })
+                    this.isUsernameRight = false;
+                    this.usernameErrorMsg = '账号格式不正确,请输入正确的账号';
                     return;
                 }
-                // else if(this.isPasswordRight && this.isUsernameRight){ 
-                //     this.login();
-                // }
             },
             checkPassword(event){
-                const password = event.target.value;
+                const password = typeof event === 'object'?event.target.value:event;
                 if(password.length < 6){
-                    this.$message({
-                        type:'error',
-                        message:'密码为数不能小于6位',
-                    })
+                    this.isPasswordRight = false;
+                    this.passwordErrorMsg = '密码为数不能小于6位';
                     return;
                 }else if(password.length > 12){
-                    this.$message({
-                        type:'error',
-                        message:'密码为数不能大于12位'
-                    })
+                    this.isPasswordRight = false;
+                    this.passwordErrorMsg = '密码为数不能大于12位';
                     return;
                 }
                 const rex = new RegExp(/^[a-z,A-Z,0-9,_]{1,12}$/);
                 this.isPasswordRight = rex.test(password);
                 if(!this.isPasswordRight){
-                    this.$message({
-                        type:'error',
-                        message:'密码格式不正确,请输入正确的密码'
-                    })
+                    this.isPasswordRight = false;
+                    this.passwordErrorMsg = '密码格式不正确,请输入正确的密码';
                     return;
-                }
-                // else if(this.isPasswordRight && this.isUsernameRight){ 
-                //     this.login();
-                // }
-            },
-            pressEnter(){
-                console.log('a')
-                if(this.isPasswordRight && this.isUsernameRight){ 
-                    this.login();
                 }
             },
             forget(){
@@ -124,9 +109,9 @@
                 })
             },
             login(){
-                // this.checkUsername(this.username);
-                // this.checkPassword(this.password);
-                if(this.isPasswordRight || this.isUsernameRight){ 
+                this.checkUsername(this.username);
+                this.checkPassword(this.password);
+                if(this.isPasswordRight && this.isUsernameRight){ 
                     this.$router.push('main');
                 }
             }
@@ -140,6 +125,7 @@
     .login-input-position
         width:500px
         height:350px
+        border-radius:5px
         margin:0 auto
         box-shadow:5px 5px 5px 5px gray
         text-align:center
@@ -159,12 +145,14 @@
                 text-align:right
             .el-input
                 width:240px
-        .login-userName
-            margin-bottom:32px
-        .login-password
-            margin-bottom:16px
+        .error-message
+            color:#f24531
+            line-height:20px
+            font-size:14px
+            height:24px
         .create-or-forget-buttonArray
             margin-bottom:16px
-            .forget-password
-                //margin-right:8px
+            .el-button 
+                padding-top:8px
+                padding-bottom:8px
 </style>
